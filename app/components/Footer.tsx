9211e21be4f266abfdbec658e16957038ac11cbd
James Stringer <jim.stringer@gmail.com>
2026-02-12 19:29:14 -0600

    moved logo to assets and fixed import

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,10 @@
+import AllStarLogo from '@/assets/star-logo.png';
+
 export const Footer = () => {
   return (
     <footer className="border-t border-white/10 bg-black/70 py-12">
       <div className="container mx-auto flex px-4 text-center font-serif text-xl">
-        <img className="h-12 w-14 flex-none" src={'/star-logo.png'} alt="logo" />
+        <img className="h-12 w-14 flex-none" src={AllStarLogo} alt="logo" />
         <div className="mx-auto flex flex-col sm:flex-row">
           <div className="p-2 text-2xl sm:p-4">All Star Tours</div>
           <div className="mx-auto p-2 text-2xl sm:p-4">204-XXX-XXXX</div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,14 +4,14 @@ import { NavLink } from '@/components/NavLink';
 // In VITE, you can import SVGs as React components using the ?react suffix for better performance.
 // Uncomment the line below to use the GitHub icon in your navbar.
 //import IconGitHub from '@/assets/github.svg?react';
-//import AllStarLogo from '@assets/star-logo.png';
+import AllStarLogo from '@/assets/star-logo.png';
 //<img src={AllStarLogo} alt="logo" />
 
 export const Navbar = () => {
   return (
     <header className="relative z-10 flex h-16 items-center justify-between px-4 md:px-8">
       <div className="flex">
-        <img className="h-8" src={'/star-logo.png'} alt="logo" />
+        <img className="h-8" src={AllStarLogo} alt="logo" />
         <Link
           to="/"
           className="text-bomber-gold -mt-1 font-serif text-xl leading-none font-bold tracking-tight md:text-2xl"
